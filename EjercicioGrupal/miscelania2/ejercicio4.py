9f738ddaccf2e0ef18be6983ab0ad32c9f600616
Sebastian <sebastiantriana1911@gmail.com>
2023-05-07 12:55:07 -0700

    Ejercicio CS

--- EjercicioGrupal/miscelania2/ejercicio4.py
+++ EjercicioGrupal/miscelania2/ejercicio4.py
@@ -1,3 +1,6 @@
+# Llenar un arreglo con la serie de Fibonacci, con la cantidad de dígitos que el usuario indique al inicio del 
+# programa.
+
 lista = []
 numero_final = int(input("Ingrese en numero que indique el final de la lista: "))
 num1 = 0
