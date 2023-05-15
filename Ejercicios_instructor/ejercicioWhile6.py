d26316422aeaee3db02edc1a2ba3e0a0290dd55a
Sebastian <sebastiantriana1911@gmail.com>
2023-05-15 07:14:10 +0200

    Prueba

--- Ejercicios_instructor/ejercicioWhile6.py
+++ Ejercicios_instructor/ejercicioWhile6.py
@@ -1,7 +1,7 @@
 # Hacer un codigo donde el programa escoja un numero aleatorio del 0 al 10 sin que el usuario sepa cual es el numero,
 # el usuario ingresara un numero, si este es menor que el numero escogido por el programa, se mostrara un mensaje 
 # que el numero es mayor al que puso, y si es mayor pues debera mostrar que el numero es menor al que ingreso, si 
-# el numero que ingrese el igual al numero escogido por el programa se le dira felicitaciones
+# el numero que ingrese el igual al numero escogido por el programa se le dira felicitaciones. 
 
 from random import randint
 incognita = randint (0,10)
