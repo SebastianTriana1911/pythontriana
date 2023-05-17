966da40333f880841e021deea3cfe969675d74e0
Sebastian <sebastiantriana1911@gmail.com>
2023-05-17 03:08:32 +0200

    Cuantiles

--- Ejercicios_instructor/ejercicio8.py
+++ Ejercicios_instructor/ejercicio8.py
@@ -8,10 +8,15 @@
 # Generar listas separadas por cuartil y quintil
 
 import random 
-lista2 = [1,2,3,4,5,6,7,8,9,10,11,12]
+tam = random.randrange(10,30)
+lista2 = [random.randrange(5,15) for i in range (tam)]
 lista1 = []
 tam = random.randrange(10,30)
 cuartil = []
+result = []
+print (lista2)
+lista2.sort()
+print (lista2)
 
 # def llenarLista (lista):
     # lista = [random.randrange(100,500) for i in range (tam)]
@@ -28,20 +33,23 @@ cuartil = []
                 # lista[j]=aux
     # return lista
 # print (ordenarLista(lista1))
-
 for i in range (1,4):
     if len(lista2) % 2 != 0:
-        cuartil1 = i * (len(lista2) + 1) / 4
-        cuartil.append(cuartil1)
-print (cuartil)
+        cuartil1 = int (i * (len(lista2) + 1) / 4)
+        posicion = lista2[cuartil1] 
+        print (f"El Q{i} esta en la posicion {cuartil1}")
+        print (f"El numero que esta en la posicion {cuartil1} es {posicion}")
+    result.append(posicion)
+print(result)
 
-for i in range (1,4):
-    if len(lista2) % 2 == 0: 
-        cuartil1 = i * (len(lista2) - 1) // 4
-        cuartil2 = lista2[cuartil1]
-        cuartil3 = lista2[cuartil1 + 1]
-        cuartil1 = cuartil2 + cuartil3
-        cuartil1 = cuartil1 / 2
-        cuartil.append(cuartil)
-print (cuartil)
+
+# for i in range (1,4):
+#     if len(lista2) % 2 == 0: 
+#         cuartil1 = i * (len(lista2) - 1) // 4
+#         cuartil2 = lista2[cuartil1]
+#         cuartil3 = lista2[cuartil1 + 1]
+#         cuartil1 = cuartil2 + cuartil3
+#         cuartil1 = cuartil1 / 2
+#         cuartil.append(cuartil)
+# print (cuartil)
  
