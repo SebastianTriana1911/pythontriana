4ad19a0e4be0a455b9b91b7b5e199fba933fde39
Sebastian <sebastiantriana1911@gmail.com>
2023-05-17 21:59:34 -0500

    Cuantiles

--- Ejercicios_instructor/ejercicio8.py
+++ Ejercicios_instructor/ejercicio8.py
@@ -20,20 +20,38 @@ def organizar_lista(lista):
     return lista
 listaOrig = organizar_lista(listaOrig)
 print (f"La lista organizada de manera ascendente {listaOrig}")
-    
+
+Q1 = []
+Q2 = []
+Q3 = []
+for i in range (1, 4):
+    posicion = i * (((len(listaOrig) + 1) / 4))
+    print(f"El Q{i} esta en la posicion {posicion}")
+    redondeo = round(posicion)
+    if posicion > redondeo:
+        resultado = (listaOrig[redondeo] + listaOrig[redondeo -1]) / 2
+        print (f"El valor del Q{i} es: {resultado}")
+    elif posicion < redondeo:
+        redondeo = redondeo - 1
+        resultado = (listaOrig[redondeo -1] + listaOrig[redondeo]) / 2
+        print (f"El valor del Q{i} es: {resultado}")
+    elif posicion == redondeo:
+        resultado = listaOrig[redondeo -1]
+        print (f"El valor del Q{i} es: {resultado}")
+    # if i == 1:
+    #     Q1.append(resultado)
+    #     resultQ1 = Q1[0]
+    #     print (Q1)
+    # if i == 2:
+    #     Q2.append(resultado)
+    #     print (Q2)
+    # if i == 3:
+    #     Q3.append(resultado)
+    #     print (Q3)
+
 # Hallar valor de cada quintil
 # Funcion que retorne el valor y la posicion dada la lista y el numero de cuartil y quintil
 # Promedio por cada cuartil 
 # Promedio por cada quintil
 # Mayor y menor por separado de cada cuartil y quintil respectivamente
 # Generar listas separadas por cuartil y quintil
-
-# def cuartil (lista):
-for i in range (1, 4):
-    posicion = i * (((len(listaOrig) + 1) / 4))
-    print(f"El Q{i} esta en la posicion {posicion}")
-    if posicion == 
-    print (f"El valor del Q{i} es: {listaOrig[posicion - 1]}")
-    # if len(listaOrig) % 2 != 0:
-    #     posicion2 = (listaOrig[posicion - 1] + listaOrig[posicion]) / 2
-    #     print (f"El valor del Q{i} es: {posicion}")
