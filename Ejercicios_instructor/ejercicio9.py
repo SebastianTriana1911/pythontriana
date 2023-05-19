2691baae68d9902f40e3c640081415f8fb668e9a
Sebastian <sebastiantriana1911@gmail.com>
2023-05-18 22:23:39 -0500

    Correccion

--- Ejercicios_instructor/ejercicio9.py
+++ Ejercicios_instructor/ejercicio9.py
@@ -11,7 +11,7 @@ for i in range (longitud):
     tupla = tupla + (numeros,)
 print(tupla)
 
-posicion = ((len(tupla) // 2)) + 1
-print (posicion)
+# posicion = ((len(tupla) // 2)) + 1
+# print (posicion)
 
 
