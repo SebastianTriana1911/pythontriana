b1c3c0545c7b00df9ce49344cf9672fc996a6027
Sebastian <sebastiantriana1911@gmail.com>
2023-05-19 20:27:35 -0500

    EjerciciosDicc

--- Ejercicios_instructor/ejercicioDic2.py
+++ Ejercicios_instructor/ejercicioDic2.py
@@ -29,9 +29,16 @@ print (f"1. Ingles a Español")
 print (f"2. Español a Ingles\n")
 respuesta = int(input("Con que funcion desea llenar su diccionario: "))
 if respuesta == 1:
-    print(inglesEspañol(int(input("Cuanteas veces desea llenan el diccionario: ")), input("Escriba la clave en ingles: "), input("Escriba el valor en español: "), diccIngles))
+    print(inglesEspañol(int(input("Cuanteas veces desea llenan el diccionario: ")), input("Escriba la clave en ingles: "), input("Escriba el valor en español: "), diccUsuario))
 elif respuesta == 2:
-    print(españolIngles(int(input("Cuanteas veces desea llenan el diccionario: ")), input("Escriba la clave en español: "), input("Escriba el valor en ingles: "), diccEspañol))
+    print(españolIngles(int(input("Cuanteas veces desea llenan el diccionario: ")), input("Escriba la clave en español: "), input("Escriba el valor en ingles: "), diccUsuario))
 else:
     print (f"La opccion que escogio no esta dentro del menu")
 
+# Codifique funciones para alamacenar en tuplas de cada diccionario todos los animales en español y en ingles respectivamente. 
+def diccTuplas (diccionario):
+    tupla = diccionario.items()
+    return tupla
+print (diccTuplas(diccEspañol))
+print (diccTuplas(diccIngles))
+print (diccTuplas(diccUsuario))
