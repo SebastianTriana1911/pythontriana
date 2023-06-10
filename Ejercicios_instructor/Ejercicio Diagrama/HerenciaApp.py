845b4ca83b5670f2418baf79b3a9b559fb3d1c17
Sebastian <sebastiantriana1911@gmail.com>
2023-06-10 01:01:09 -0500

    Ejercicio Diagrama

--- Ejercicios_instructor/Ejercicio Diagrama/HerenciaApp.py
+++ Ejercicios_instructor/Ejercicio Diagrama/HerenciaApp.py
@@ -2,20 +2,32 @@ from Cliente import *
 from Productos import *
 from Individual import *
 
+# Se instancian objetos de la clase Cliente
 persona = Cliente(10,"Normal","individual","hola")
 persona2 = Cliente(20,"Normal","empresa","hola")
-persona3 = Individual (20,"persona3","Indi","hola","sebas@gmail.com", 313802, "xra2" )
+
+# Al objeto persona se le añade los metodos getDatos para visualizar todos los datos y el setId para modificar el Id
 print (persona.getDatos())
 print (persona.setId(11))
 print (persona.getDatos())
 
+# Se instancian 2 objetos ob1 y ob2 de la clase Producto 
 ob1 = Producto(1,"Arroz","Grano","Grasas",1000)
 ob2 = Producto(2,"Lenteja","Grano","Engorda",2000)
+
+# A los dos objetos previamente instanciados se le asignan el metodo getProducto que viene de la clase Producto, estos 
+# dos prints nos retornaran el valor de cada uno de los objetos
 print(ob1.getProducto())
 print(ob2.getProducto())
 
+# Al objeto persona instanciado de la clase Cliente se le agrega el metodo agregarProducto con el parametro de 
+# un producto, en este caso le pasaremos el objeto ob1 y ob2 creado desde la clase Producto para que lo añada en la lista de 
+# productos del objeto al que se le esta pasando el metodo, lo que sucedera va a ser que se va añadir ob1 y ob2 a la 
+# lista producto como ya lo habiamos dicho y nos mostrara es la direccion de memoria ya que por el momento ob1 y ob2 son 
+# simplemente dos objetos
 persona.agregarProducto(ob1)
 persona.agregarProducto(ob2)
+
 
 for producto in persona.getProductos():
     print (f"AGREGACION {producto.getProducto()}")
@@ -38,5 +50,5 @@ print(persona3.descuentoProducto(1500))
 
 
 
-
+#persona3 = Individual (20,"persona3","Indi","hola","sebas@gmail.com", 313802, "xra2" )
 
--- Ejercicios_instructor/Ejercicio Diagrama/Productos.py
+++ Ejercicios_instructor/Ejercicio Diagrama/Productos.py
@@ -10,7 +10,7 @@ class Producto:
     def getProducto (self):
         return f"{self.__id} {self.__nombre} {self.__tipo} {self.__descripcion} {self.__precio}"
     
-    def descuentoProducto (self,precio):
+    #def descuentoProducto (self,precio):
         self.__precio = precio
         precio = self.__precio * 0.03
         return f"El precio con descuento para un individual es: {precio} "
